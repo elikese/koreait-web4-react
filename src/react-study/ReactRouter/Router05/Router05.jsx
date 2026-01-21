0eceade8673deaa7b6d756c38df9bc6b89faefce
elikese <elikese@gmail.com>
2026-01-21 21:46:58 +0900

    feat: zustand 도입

--- src/react-study/ReactRouter/Router05/Router05.jsx
+++ src/react-study/ReactRouter/Router05/Router05.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, useNavigate, useSearchParams } from 'react-router-dom'
 
 export default function Router05() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/' element={<OrderList />}/>
+        <Route path='/order' element={<OrderDetail />} />
       </Routes>
     </BrowserRouter>
   )
@@ -25,8 +26,18 @@ function OrderList() {
       <div>
         {/* orders를 map을사용하여 카드형식으로 뿌려주세요
           상품이름만 보이면 됩니다!
+          해당 이름 클릭시 OrderDetail 컴포넌트 렌더링
+          이때 props로 데이터전달x -> URL로 데이터 전달
         */}
-        {}
+        {orders.map((o) => {
+          const {product, quantity, status} = o;
+          const url = `/order?product=${product}&quantity=${quantity}&status=${status}`;
+          return (
+            <div>
+              <Link to={url}>{product}</Link>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
@@ -36,15 +47,23 @@ function OrderList() {
 function OrderDetail() {
   // props가 아니라 url로 데이터를 전달받아
   // 각 정보를 렌더링해주세요
+  const [searchParams] = useSearchParams();
+  const navigator = useNavigate();
+
+  // 쿼리스트링에서 뽑아온 데이터 -> string
+  const product = searchParams.get("product")
+  const quantity = searchParams.get("quantity")
+  const status = searchParams.get("status")
+  
   return (
     <div>
       <h1>주문 상세 페이지</h1>
       <div>
-        <p>상품명:</p>
-        <p>주문 수량:</p>
-        <p>배송 상태:</p>
+        <p>상품명:{product}</p>
+        <p>주문 수량:{quantity}</p>
+        <p>배송 상태:{status}</p>
       </div>
-      <button>목록으로</button>
+      <button onClick={() => navigator("/")}>목록으로</button>
     </div>
   )
 }
