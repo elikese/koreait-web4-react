28997a64e3ee6faf39d5b68eec8eaac10cec8863
elikese <elikese@gmail.com>
2026-02-05 21:14:29 +0900

    feat: login

--- src/mini-project/pages/Signin/Signin.jsx
+++ src/mini-project/pages/Signin/Signin.jsx
@@ -13,7 +13,17 @@ export default function Signin() {
   const {isPending, mutate} = useSigninMutation();
 
   const handleSignin = () => {
-  
+    const {username, password} = formVal;
+    if(!username || !password) {
+      // 토스트
+      return;
+    }
+    mutate(formVal, {
+      onSuccess: () => {
+        // 새로고침 + 홈url로
+        window.location.href = "/"
+      }
+    })
   }
 
   return (
@@ -44,7 +54,11 @@ export default function Signin() {
               placeholder="비밀번호를 입력하세요"
             />
           </div>
-          <button css={s.btn} onClick={handleSignin}>로그인</button>
+          <button 
+            css={s.btn} 
+            onClick={handleSignin}
+            disabled={isPending}
+          >{isPending ? "로그인 중" : "로그인"}</button>
           <div css={s.signupTextBox}>
             <span css={s.signupText}>계정이 없으신가요?</span>
             <Link css={s.signupLink} to="/signup">회원가입</Link>
