d04a24f3b84b59fd2b58b8c5e491129f3ccf0284
elikese <elikese@gmail.com>
2026-02-06 21:29:09 +0900

    feat: mypage useQuery

--- src/mini-project/pages/MyPage/components/FormInput/FormInput.jsx
+++ src/mini-project/pages/MyPage/components/FormInput/FormInput.jsx
@@ -1,33 +1,56 @@
 import { useQueryClient } from '@tanstack/react-query';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from '../../../../hooks/useForm';
 
 export default function FormInput({isPasswordMode, setIsPasswordMode}) {
   const [isEditMode, setIsEditMode] = useState(false);
   const queryClient = useQueryClient();
   // useQuery쿼리키를 통해 캐싱된 전역데이터 가져오기 ok
   const user = queryClient.getQueryData(["getMyInfo"]);
-  
+  const {formVal, handleChange, setFormVal} = useForm({
+    name: "",
+    email: ""
+  });
+
+  // user가 로드되는데 걸리는 시간존재
+  useEffect(() => {
+    if(!!user) {
+      setFormVal({
+        name: user.name,
+        email: user.email
+      })
+    }
+  }, [user])
 
 
   return (
     <>
       <div>
         <label>이름</label>
-        <input type="text" />
+        <input 
+          type="text" 
+          name='name'
+          value={formVal.name}
+          onChange={handleChange}
+          placeholder='이름을 입력하세요'
+          disabled={!isEditMode}
+        />
       </div>
       <div>
         <label>이메일</label>
-        <input type="text" />
+        <input 
+          type="email"
+          name='email'
+          value={formVal.email}
+          onChange={handleChange}
+          placeholder='이메일을 입력하세요'
+          disabled={!isEditMode}
+        />
       </div>
       <button onClick={() => setIsPasswordMode(true)}>비밀번호 변경</button>
       {
         isEditMode
-        ? <button 
-            onClick={() => setIsEditMode(true)}
-          >
-            수정하기
-          </button>
-        : (
+        ? (
           <div>
             <button 
               onClick={() => setIsEditMode(false)}
@@ -37,6 +60,11 @@ export default function FormInput({isPasswordMode, setIsPasswordMode}) {
             <button>수정완료</button>
           </div>
         )
+        : <button 
+            onClick={() => setIsEditMode(true)}
+          >
+            수정하기
+          </button>
       }
     </>
   )
